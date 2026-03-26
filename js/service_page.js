80a2f2dfab13324b2832784eafb29c7533b155cd
erik <ejanssen@itmatters.nl>
2026-03-26 16:38:11 +0100

    Forgot in prev commit

--- js/service_page.js
+++ js/service_page.js
@@ -1,30 +1,27 @@
 'use strict';
 
-let error_msg = null;
-
 function success_callback(target_id)
 {
+   showSuccess(`Connected to ${target_id}`);
 }
 
 function error_callback(error_code, error_reason)
 {
-   error_msg.html(`An error occurred. Code: {error_code}. Reason: {error_reason}`)
+   showError(`An error occurred. Code: ${error_code}. Reason: ${error_reason}`)
 }
 
 $(document).ready(function()
 {
-   let err_msg = $('#error');
    let c = $('#device_list');
    let org_id = c.attr('data-org');
    let service_id = c.attr('data-service');
    let client_id = c.attr('data-client');
 
-   const webrtc = new WebRTCClient(org_id, client_id, 'remoteVideo', success_callbak, error_callback);
+   const webrtc = new WebRTCClient(org_id, client_id, 'remoteVideo', success_callback, error_callback);
 
    $('.cam-button').click(function()
    {
       let mac = $(this).attr('data-cam');
-      error_msg.html('');
       $.ajax({
          method: 'GET',
          url: Urls.p_service_token(service_id)
